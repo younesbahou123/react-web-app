85f3bc606f077ebe5529976f0a730a5e6c320ba0
younesbahou123 <myeurikateam@gmail.com>
2025-01-27 14:24:59 -0500

    first commit

---
+++ src/components/title.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ padding: "10px", backgroundColor: "#f8b400", textAlign: "center" }}>
+      <h1>My first Food Delivery App available to order food from diverse restaurants</h1>
+    </header>
+  );
+};
+
+export default Header;
